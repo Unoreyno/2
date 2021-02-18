5f5481104cc23264237e650d58ce452f36574c0f
Unoreyno <krzysztof.michalski333@gmail.com>
2021-02-18 14:21:43 +0100

    dzialajacy k p n

--- src/kal.java
+++ src/kal.java
@@ -14,14 +14,21 @@ public class kal {
 
         Scanner Scanner = new Scanner(System.in);
         
-       
-        System.out.println("Kliknij enter");
+        
+        int wynikW = 0;
+        int wynikP = 0;
+        int wynikR = 0;
+
+        System.out.println("Kliknij ENTER by zagrać w papier, kamień, nożyce");
         Scanner.nextLine();
+        boolean gra = true;
+
+        do{
         System.out.println("Napisz kamień (k), papier (p) albo nożyce (n)");
         String znak = Scanner.nextLine();
 
         
-        int wynik = 0;
+        
 
         
         switch(znak){
@@ -30,11 +37,13 @@ public class kal {
                 int zk = r.nextInt(3);
                 if(zk == 0){
                     System.out.println("Wylosowałem kamień, zremisowaliśmy");
-                
+                    wynikR =+ 1;
                 }else if(zk == 1){
                     System.out.println("Wylosowałem papier, przegrałeś");
+                    wynikP =+ 1;
                 }else{
                     System.out.println("Wylosowałem nożyce, wygrałeś");
+                    wynikW =+ 1;
                 }
 
             break;
@@ -43,11 +52,14 @@ public class kal {
                 int zp =r.nextInt(3);
                 if(zp == 0){
                     System.out.println("Wylosowałem kamień, wygrałeś");
+                    wynikW =+ 1;
                 
                 }else if(zp == 1){
                     System.out.println("Wylosowałem papier, zremisowaliśmy");
+                    wynikR =+ 1;
                 }else{
                     System.out.println("Wylosowałem nożyce, przegrałeś");
+                    wynikP =+ 1;
                 }
 
             break;
@@ -56,23 +68,47 @@ public class kal {
                 int zn =r.nextInt(3);
                 if(zn == 0){
                     System.out.println("Wylosowałem kamień, przegrałeś");
+                    wynikP =+ 1;
                 
                 }else if(zn == 1){
                     System.out.println("Wylosowałem papier, wygrałeś");
+                    wynikW =+ 1;
                 }else{
                     System.out.println("Wylosowałem nożyce, zremisowaliśmy");
+                    wynikR =+ 1;
                 }
 
             break;
             
             default:
 
-                System.out.println("Co innego");
+                System.out.println("Zły znak");
             break;
         }
-        System.out.println("Czy chcesz zagrać jeszcze raz? Napisz tak lub nie");
-            String odp = Scanner.nextLine();
         
+
+            Scanner.nextLine();
+            System.out.println("Czy chcesz zagrać jeszcze raz? Napisz tak lub nie (Może być też t lub n)");
+            String odpo = Scanner.nextLine();
+
+            if (odpo.equals("tak")){
+                gra = true;
+            }else if (odpo.equals("t")){
+                gra = true;
+            }else{
+                gra = false;
+            }
+
+            
+
+    }while(gra);
+        
+
+        
+        // System.out.println("Wygrałeś "+wynikW+" razy, przegrałeś "+wynikP+" razy, a "+wynikR+" razy zremisowaliśmy");
+        System.out.println("Wygrane: "+wynikW);
+        System.out.println("Przegrane: "+wynikP);
+        System.out.println("Remisy: "+wynikR);
         
     
         
